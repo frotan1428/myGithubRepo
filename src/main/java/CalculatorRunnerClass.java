ca56c452e78a53f1a27f4d2f13bf6aa736c6cd0d
Senem Aydın <dytsenemcelik@gmail.com>
2023-06-04 19:18:06 +0300

    final update

--- src/main/java/CalculatorRunnerClass.java
+++ src/main/java/CalculatorRunnerClass.java
@@ -6,7 +6,8 @@ public class CalculatorRunnerClass {
         calculator.add(4,6);
         calculator.subtract(4,2);
         System.out.println( calculator.multiply(10,5));
-        System.out.println("This is the last commit");
+        System.out.println("This is new update");
+        System.out.println("This is second update");
 
     }
 }
